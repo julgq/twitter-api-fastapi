4377b7aea9149461066d3947e597d3cbcc52a7fe
Julio Gtz <jc.gutierrezq@gmail.com>
2021-11-19 11:29:36 -0700

    path operations users

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from uuid import UUID
 from datetime import date
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 # Pydantic
 from pydantic import BaseModel
@@ -12,6 +12,7 @@ from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI
+from fastapi import status
 
 
 app = FastAPI()
@@ -32,11 +33,96 @@ class User(UserBase):
 class Tweet(BaseModel):
 	tweet_id: UUID = Field(...)
 	content: str = Field(..., max_length=256, min_length=1)
-	created_at: datetime = Field(default=date.now())
+	created_at: datetime = Field(default=datetime.now())
 	update_at: Optional[datetime] = Field(default=None)
 	by: User = Field(...)
 
+# Path Operations
 
 @app.get(path="/")
 def home():
 	return {"Twitter API": "working!"}
+
+## Users
+
+@app.post(
+	path="/signup",
+	response_model=User,
+	status_code=status.HTTP_201_CREATED,
+	summary="Register a User",
+	tags=['Users']
+
+)
+def signup():
+	pass
+
+
+@app.post(
+	path="/login",
+	response_model=User,
+	status_code=status.HTTP_200_OK,
+	summary="Login a user",
+	tags=['Users']
+
+)
+def login():
+	pass
+
+
+@app.get(
+	path="/users",
+	response_model=List[User], # responde con un formato de lista.
+	status_code=status.HTTP_200_OK,
+	summary="Show all users",
+	tags=['Users']
+
+)
+def sow_all_users():
+	pass
+
+@app.get(
+	path="/users/{user_id}",
+	response_model=User, # responde con un formato de lista.
+	status_code=status.HTTP_200_OK,
+	summary="Show a user",
+	tags=['Users']
+
+)
+def sow_a_user():
+	pass
+
+@app.get(
+	path="/users/{user_id}",
+	response_model=User, # responde con un formato de lista.
+	status_code=status.HTTP_200_OK,
+	summary="Show a user",
+	tags=['Users']
+
+)
+def sow_a_user():
+	pass
+
+@app.delete(
+	path="/users/{user_id}/delete",
+	response_model=User, # responde con un formato de lista.
+	status_code=status.HTTP_200_OK,
+	summary="Delete a user",
+	tags=['Users']
+
+)
+def delete_a_user():
+	pass
+
+
+@app.put(
+	path="/users/{user_id}/update",
+	response_model=User, # responde con un formato de lista.
+	status_code=status.HTTP_200_OK,
+	summary="Update a user",
+	tags=['Users']
+
+)
+def update_a_user():
+	pass
+
+## Tweets
