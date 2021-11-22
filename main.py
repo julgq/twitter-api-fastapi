452831a2e4c5d074736c5e0a21df814aa6a9e691
Julio Gtz <jc.gutierrezq@gmail.com>
2021-11-21 21:28:53 -0700

    show users

--- main.py
+++ main.py
@@ -106,7 +106,24 @@ def login():
 
 )
 def sow_all_users():
-	pass
+	"""
+	This path operation show all users in the app
+
+	Parameters:
+		-
+
+	Returns a json list with all users in the app, with the following keys:
+		- user_id: UUID
+		- email: Emailstr
+		- first_name: str
+		- lat_name: str
+		- birth_date: datetime
+	"""
+
+	with open("users.json", "r+", encoding="utf-8") as f:
+		results = json.loads(f.read())
+		return results
+
 
 ### Show a user
 @app.get(
